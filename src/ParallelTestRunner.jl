f4be9580d0cf8e5035ea6b80f1a1bdf5f537a3d0
Valentin Churavy <v.churavy@gmail.com>
2025-10-07 16:13:35 +0200

    remove setup.jl

--- src/ParallelTestRunner.jl
+++ src/ParallelTestRunner.jl
@@ -10,8 +10,6 @@ using Base.Filesystem: path_separator
 import Test
 import Random
 
-include("setup.jl")
-
 #Always set the max rss so that if tests add large global variables (which they do) we don't make the GC's life too hard
 if Sys.WORD_SIZE == 64
     const JULIA_TEST_MAXRSS_MB = 3800
@@ -58,6 +56,81 @@ function with_testset(f, testset)
         end
     end
     return nothing
+end
+
+if VERSION >= v"1.13.0-DEV.1044"
+    using Base.ScopedValues
+end
+
+## entry point
+
+function runtest(f, name)
+    function inner()
+        # generate a temporary module to execute the tests in
+        mod_name = Symbol("Test", rand(1:100), "Main_", replace(name, '/' => '_'))
+        mod = @eval(Main, module $mod_name end)
+        @eval(mod, import ParallelTestRunner: Test, Random)
+        @eval(mod, using .Test, .Random)
+
+        let id = myid()
+            wait(@spawnat 1 print_testworker_started(name, id))
+        end
+
+        ex = quote
+            GC.gc(true)
+            Random.seed!(1)
+
+            res = @timed @testset $name begin
+                Main.include($f)
+            end
+            res..., 0, 0, 0
+        end
+        data = Core.eval(mod, ex)
+        #data[1] is the testset
+
+        # process results
+        rss = Sys.maxrss()
+        if VERSION >= v"1.11.0-DEV.1529"
+            tc = Test.get_test_counts(data[1])
+            passes, fails, error, broken, c_passes, c_fails, c_errors, c_broken =
+                tc.passes, tc.fails, tc.errors, tc.broken, tc.cumulative_passes,
+                tc.cumulative_fails, tc.cumulative_errors, tc.cumulative_broken
+        else
+            passes, fails, errors, broken, c_passes, c_fails, c_errors, c_broken =
+                Test.get_test_counts(data[1])
+        end
+        if data[1].anynonpass == false
+            data = (
+                (passes + c_passes, broken + c_broken),
+                data[2],
+                data[3],
+                data[4],
+                data[5],
+                data[6],
+                data[7],
+                data[8],
+            )
+        end
+        res = vcat(collect(data), rss)
+
+        GC.gc(true)
+        return res
+    end
+
+    res = @static if VERSION >= v"1.13.0-DEV.1044"
+        @with Test.TESTSET_PRINT_ENABLE => false begin
+            inner()
+        end
+    else
+        old_print_setting = Test.TESTSET_PRINT_ENABLE[]
+        Test.TESTSET_PRINT_ENABLE[] = false
+        try
+            inner()
+        finally
+            Test.TESTSET_PRINT_ENABLE[] = old_print_setting
+        end
+    end
+    return res
 end
 
 function runtests(ARGS, testfilter = _ -> true)
@@ -169,7 +242,14 @@ function runtests(ARGS, testfilter = _ -> true)
 
         return withenv("JULIA_NUM_THREADS" => 1, "OPENBLAS_NUM_THREADS" => 1) do
             procs = addprocs(X; exename = exename, exeflags = test_exeflags, kwargs...)
-            @everywhere procs include($(joinpath(@__DIR__, "setup.jl")))
+            Distributed.remotecall_eval(
+                Main, procs, quote
+                    import ParallelTestRunner
+                    # TODO: Should we import Test for the user here?
+                    import ParallelTestRunner: Test
+                    using .Test
+                end
+            )
             if ispath(joinpath(WORKDIR, "setup.jl"))
                 @everywhere procs include($(joinpath(WORKDIR, "setup.jl")))
             end
@@ -311,7 +391,7 @@ function runtests(ARGS, testfilter = _ -> true)
                         # run the test
                         running_tests[test] = now()
                         try
-                            resp = remotecall_fetch(__runtests, wrkr, test_runners[test], test)
+                            resp = remotecall_fetch(runtest, wrkr, test_runners[test], test)
                         catch e
                             isa(e, InterruptException) && return
                             resp = Any[e]
